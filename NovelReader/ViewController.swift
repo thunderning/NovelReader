390d15bef17a896324ea8aec1df1a08ef03ae02d
thunderning <498279281@qq.com>
2017-12-01 16:13:37 +0800

    Initial Commit

---
+++ NovelReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NovelReader
+//
+//  Created by 吴佳玮 on 2017/12/1.
+//  Copyright © 2017年 吴佳玮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
