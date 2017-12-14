f71a7b360ea3396d56b3ada409e6ee076f78e752
thunderning <498279281@qq.com>
2017-12-14 15:53:23 +0800

    add readme

---
+++ NovelReader/BookCategory/BookCategoryItem.swift
@@ -0,0 +1,18 @@
+//
+//  BookCategoryItem.swift
+//  NovelReader
+//
+//  Created by 吴佳玮 on 2017/12/11.
+//  Copyright © 2017年 吴佳玮. All rights reserved.
+//
+
+import Foundation
+struct BookCategoryItem : Codable {
+    let _id:String
+    let title:String
+    let cover:String       //图片地址
+    let shortTitle:String
+}
+
+
+
